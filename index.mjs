bac55f958f9c5e211e0449dc27d5dd79b6fbd340
Jaimik Patel <jaimik1603@gmail.com>
2024-11-20 15:36:54 +0530

    name change

--- index.mjs
+++ index.mjs
@@ -1,9 +1,9 @@
-const express = require('express');
-// import express from 'express';
-const bodyParser = require('body-parser');
-// import bodyParser from 'body-parser';
-const fetch = require('node-fetch');
-// import fetch from 'node-fetch';
+// const express = require('express');
+import express from 'express';
+// const bodyParser = require('body-parser');
+import bodyParser from 'body-parser';
+// const fetch = require('node-fetch');
+import fetch from 'node-fetch';
 
 const app = express();
 app.use(bodyParser.json());
