3869935457ab6cdda31c8e7f5da0eba76a4def41
Jaimik Patel <jaimik1603@gmail.com>
2024-11-23 18:25:54 +0530

    Temp

--- verify.js
+++ verify.js
@@ -1,14 +1,14 @@
 const axios = require('axios');
 
 // Replace with your actual certificate string
-const CERTIFICATE_STRING = `CnMKLwjVwbftsLHUAxIGZW50OndhIhZaIFBsdXMgU2VjdXJpdHkgU3lzdGVtULnRgboGGkDLRt0l7VirX7Znsd2dptsKtyVyo6mClkmVfgVVlPJz4evryW8r9cj+eRIF54j6CtQfHP2Yf7uSFwlMWScVowsMEi5tRQHGh5C/U+BEh7ORr2wqnFjn5lvF2KSXTTtOrTy8bc5i8NnTJkYTy2ZAKWNb`;
+const CERTIFICATE_STRING = `CnQKMAi/zbW3kNXSAhIGZW50OndhIhdaIFBsdXMgU2VjdXJpdHkgU3lzdGVtc1DQmIe6BhpA9s+f3taaOftj7K1T20/pf5Ht+EjdKWJMgzI8MZWDPFkJk2rOM74Z2IAE8D/Cx8QQ7q4ErDxDtG8tXMLGVY0gDxIubUN+kLfAk0PgRIezka9sKpxY5+ZbxNjN3ks7Tq085v0LpDy8XU8cwR6NusR1lg==`;
 
 // Replace with your actual access token and registration endpoint
-const ACCESS_TOKEN = 'EAAZAZCxUCCBOwBOZCzgQAesnRKEY6VZC5VDP249lmpHmVVgg8RWhDcOXR6wZBHcif7hY6UoO8FUW4s7xaFDmoUh1kYNDzlqRSjomdyVH0CyMZCYPruxwZBZBTqL1ZB1yTf0DfdyhErZBUmH89dJdXuROVyes8ab6D58O6yZAtnVW1IgAZBIZCXzJJWaM2AcuSZCpD2am60fwZDZD';
-const REGISTRATION_URL = 'https://graph.facebook.com/v21.0/477412112126570/register';
+const ACCESS_TOKEN = 'EAAZAZCxUCCBOwBO2WXCG34Q1oZBQA9VKatUpZAuFhqeXsMB2TY3VufqoG4R7F8zXxi1VozwMzZCaHoa2xGGiXpZCSwYkGubYHslM0VeoBAyVEBLe7CRfuiiuBb2iaYWaVDFmUJGqDGXiD3CpozwfqZCJjfKGvEYrksMqQZBoxWBxv1PAQhihfD4vIApZCZCvjusQPqdQZDZD';
+const REGISTRATION_URL = 'https://graph.facebook.com/v21.0/469434999592396/register';
 
 // Replace with the actual 6-digit PIN sent to your phone
-const PIN = '162004';
+const PIN = '618255';
 
 // Define the request payload
 const payload = {
