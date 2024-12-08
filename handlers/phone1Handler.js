957fc8d852cb4e1471568c8f6291e7482fcfac72
Jaimik Patel <jaimik1603@gmail.com>
2024-12-08 18:24:55 +0530

    ss

--- handlers/phone1Handler.js
+++ handlers/phone1Handler.js
@@ -364,16 +364,6 @@ async function sendInteractiveMessage(to, vehicleDetails) {
       recipient_type: "individual",
       to, // The recipient's phone number
       type: "interactive",
-      // text: {
-      //   body: `
-      //     Vehicle Number: ${formattedVehicleNumber || "N/A"}
-      //     \nLatitude: ${deviceId || "N/A"}
-      //     \nLongitude: ${agency || "N/A"}
-      //     \nSpeed: ${subAgency || "N/A"}
-      //     \nReceived Date: ${receivedDate || "N/A"}
-      //     \nServer Time: ${serverTime || "N/A"}
-      //   `,
-      // },
       interactive: {
         type: "button",
         header: {
@@ -381,14 +371,12 @@ async function sendInteractiveMessage(to, vehicleDetails) {
           text: "Vehicle Information",
         },
         body: {
-          text: {
-              body: `Vehicle Number: ${formattedVehicleNumber || "N/A"}
-                \nLatitude: ${deviceId || "N/A"}
-                \nLongitude: ${agency || "N/A"}
-                \nSpeed: ${subAgency || "N/A"}
-                \nReceived Date: ${receivedDate || "N/A"}
-                \nServer Time: ${serverTime || "N/A"}`,
-            },
+          text: `Vehicle Number: ${
+            formattedVehicleNumber || "N/A"
+          }\nLatitude: ${deviceId || "N/A"}\nLongitude: ${agency || "N/A"}
+                \nSpeed: ${subAgency || "N/A"}\nReceived Date: ${
+            receivedDate || "N/A"
+          }\nServer Time: ${serverTime || "N/A"}`,
         },
         footer: {
           text: "Tap to update.",
