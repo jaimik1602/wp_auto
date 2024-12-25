44c6145c372290c79b0039188a23d7744b58a5c7
Jaimik Patel <jaimik1603@gmail.com>
2024-12-25 20:00:38 +0530

    cc

--- handlers/phone1Handler.js
+++ handlers/phone1Handler.js
@@ -70,7 +70,6 @@ exports.handleMessage = async (req, res) => {
   const text = message.text?.body?.trim();
   const currentWeek = getCurrentWeek();
 
-  // console.log("Handling message for Phone 1:", text);
   // Save the number and WhatsApp name to the database
   var temp = await saveContactToDatabase(from, name);
 
@@ -79,100 +78,137 @@ exports.handleMessage = async (req, res) => {
   const userState = userSessions[from];
 
   try {
+    // Check if the sender is blocked
+    const isBlocked = await checkBlockStatus(from); // Replace with your blocklist function
+    if (isBlocked) {
+      console.log(`blocked:- ${from}`);
+      return; // Stop further processing
+    }
     console.log(`Sender:- ${from} And Msg:- ${text}`);
-    if (from == 919727668616) {
-    } else {
-      if (
-        // userState.step === 0 &&
-        typeof text === "string" &&
-        text.toLowerCase() === "hi"
-      ) {
-        // resetUserState(from);
-        await sendWhatsAppMessage(
-          from,
-          "Please enter your vehicle number.",
-          "en"
-        );
-        await sendWhatsAppMessage(
-          from,
-          "कृपया अपनी वाहन संख्या दर्ज करें।",
-          "hi"
-        );
-        await sendWhatsAppMessage(
-          from,
-          "કૃપયા તમારો વાહન નંબર દાખલ કરો.",
-          "gu"
-        );
-        userState.step = 1;
-      } else if (typeof text === "string" && text.toLowerCase() == "stop") {
-        resetUserState(from);
-      } else if (userState.step === 1) {
-        const formattedVehicleNumber = formatVehicleNumber(text);
-        const phoneNumber = from; // Assuming 'from' contains the user's mobile number
-        console.log(
-          `Vehicle Number: ${formattedVehicleNumber}, Phone Number: ${phoneNumber}`
-        );
+    if (
+      // userState.step === 0 &&
+      typeof text === "string" &&
+      text.toLowerCase() === "hi"
+    ) {
+      // resetUserState(from);
+      await sendWhatsAppMessage(
+        from,
+        "Please enter your vehicle number.",
+        "en"
+      );
+      await sendWhatsAppMessage(
+        from,
+        "कृपया अपनी वाहन संख्या दर्ज करें।",
+        "hi"
+      );
+      await sendWhatsAppMessage(from, "કૃપયા તમારો વાહન નંબર દાખલ કરો.", "gu");
+      userState.step = 1;
+    } else if (typeof text === "string" && text.toLowerCase() == "stop") {
+      resetUserState(from);
+    } else if (userState.step === 1) {
+      const formattedVehicleNumber = formatVehicleNumber(text);
+      const phoneNumber = from; // Assuming 'from' contains the user's mobile number
+      console.log(
+        `Vehicle Number: ${formattedVehicleNumber}, Phone Number: ${phoneNumber}`
+      );
 
-        const response = await fetchVehicle(
-          formattedVehicleNumber,
-          phoneNumber
-        );
+      const response = await fetchVehicle(formattedVehicleNumber, phoneNumber);
 
-        if (!response.success || !response.data[0]?.deviceid) {
-          if (response.message == "expiry") {
+      if (!response.success || !response.data[0]?.deviceid) {
+        if (response.message == "expiry") {
+          resetUserState(from);
+          await sendWhatsAppMessage(
+            from,
+            "Vehicle Recharge is over!!!\nContact on this number :- +91 88662 65662",
+            "en"
+          );
+          await sendWhatsAppMessage(
+            from,
+            "वाहन रिचार्ज ख़त्म!!!\nइस नंबर पर संपर्क करें:- +91 88662 65662",
+            "hi"
+          );
+          await sendWhatsAppMessage(
+            from,
+            "વાહન રિચાર્જ સમાપ્ત થઈ ગયું છે !!!\nઆ નંબર પર સંપર્ક કરો:- +91 88662 65662",
+            "gu"
+          );
+        } else {
+          userState.vehicleAttempts += 1;
+          if (userState.vehicleAttempts >= 3) {
             resetUserState(from);
             await sendWhatsAppMessage(
               from,
-              "Vehicle Recharge is over!!!\nContact on this number :- +91 88662 65662",
+              "You have exceeded the allowed attempts. Send 'Hi' to start the conversation.",
               "en"
             );
             await sendWhatsAppMessage(
               from,
-              "वाहन रिचार्ज ख़त्म!!!\nइस नंबर पर संपर्क करें:- +91 88662 65662",
+              "आपने अनुमत प्रयासों को पार कर लिया है। 'Hi' भेजकर बातचीत शुरू करें।",
               "hi"
             );
             await sendWhatsAppMessage(
               from,
-              "વાહન રિચાર્જ સમાપ્ત થઈ ગયું છે !!!\nઆ નંબર પર સંપર્ક કરો:- +91 88662 65662",
+              "તમે અનુમતિ આપેલા પ્રયત્નો પાર કરી દીધા છે. 'Hi' મોકલીને સંવાદ શરૂ કરો.",
               "gu"
             );
           } else {
-            userState.vehicleAttempts += 1;
-            if (userState.vehicleAttempts >= 3) {
-              resetUserState(from);
-              await sendWhatsAppMessage(
-                from,
-                "You have exceeded the allowed attempts. Send 'Hi' to start the conversation.",
-                "en"
-              );
-              await sendWhatsAppMessage(
-                from,
-                "आपने अनुमत प्रयासों को पार कर लिया है। 'Hi' भेजकर बातचीत शुरू करें।",
-                "hi"
-              );
-              await sendWhatsAppMessage(
-                from,
-                "તમે અનુમતિ આપેલા પ્રયત્નો પાર કરી દીધા છે. 'Hi' મોકલીને સંવાદ શરૂ કરો.",
-                "gu"
-              );
-            } else {
-              await sendWhatsAppMessage(
-                from,
-                `Enter Correct Vehicle Number!!!`,
-                "en"
-              );
-              await sendWhatsAppMessage(from, `सही वाहन नंबर दालीये!!!`, "hi");
-              await sendWhatsAppMessage(
-                from,
-                `સાચો વાહન નંબર દાખલ કરો!!!`,
-                "gu"
-              );
-            }
+            await sendWhatsAppMessage(
+              from,
+              `Enter Correct Vehicle Number!!!`,
+              "en"
+            );
+            await sendWhatsAppMessage(from, `सही वाहन नंबर दालीये!!!`, "hi");
+            await sendWhatsAppMessage(from, `સાચો વાહન નંબર દાખલ કરો!!!`, "gu");
           }
+        }
+      } else {
+        var userlevel = await checkUserLevel(phoneNumber);
+        //user level check
+        if (userlevel.user_level) {
+          userState.vehicleNumber = formattedVehicleNumber;
+          userState.imei = response.data[0].deviceid;
+          userState.agency = response.data[0].agency;
+          userState.subagency = response.data[0].subagency;
+          // if (
+          //   userState.subagency == "GaneshAtlanta" ||
+          //   userState.subagency == "MARUTI"
+          // ) {
+          //   resetUserState(from);
+          //   await sendWhatsAppMessage(
+          //     from,
+          //     "Subagency is restricted. For better service, please contact 88662 65662 on WhatsApp.",
+          //     "en"
+          //   );
+          //   await sendWhatsAppMessage(
+          //     from,
+          //     "सब एजेंसी प्रतिबंधित है। बेहतर सेवा के लिए कृपया 88662 65662 पर WhatsApp पर संपर्क करें।",
+          //     "hi"
+          //   );
+          //   await sendWhatsAppMessage(
+          //     from,
+          //     "સબએજન્સી પ્રતિબંધિત છે. વધુ સારી સેવા માટે, કૃપા કરીને WhatsApp પર 88662 65662 પર સંપર્ક કરો.",
+          //     "gu"
+          //   );
+          // } else {
+          await sendInteractiveMessage(from, [
+            formattedVehicleNumber,
+            response.data[0].lattitude,
+            response.data[0].longitude,
+            response.data[0].speed,
+            response.data[0].received_Date,
+            response.data[0].servertime,
+          ]);
+          userState.step = 2;
+          // }
         } else {
-          var userlevel = await checkUserLevel(phoneNumber);
-          //user level check
-          if (userlevel.user_level) {
+          //
+          var result = await weekCheck(
+            formattedVehicleNumber,
+            phoneNumber,
+            currentWeek,
+            userlevel.vehicle_count
+          );
+          if (result) {
             userState.vehicleNumber = formattedVehicleNumber;
             userState.imei = response.data[0].deviceid;
             userState.agency = response.data[0].agency;
@@ -209,158 +245,113 @@ exports.handleMessage = async (req, res) => {
             userState.step = 2;
             // }
           } else {
-            //
-            var result = await weekCheck(
-              formattedVehicleNumber,
-              phoneNumber,
-              currentWeek,
-              userlevel.vehicle_count
-            );
-            if (result) {
-              userState.vehicleNumber = formattedVehicleNumber;
-              userState.imei = response.data[0].deviceid;
-              userState.agency = response.data[0].agency;
-              userState.subagency = response.data[0].subagency;
-              // if (
-              //   userState.subagency == "GaneshAtlanta" ||
-              //   userState.subagency == "MARUTI"
-              // ) {
-              //   resetUserState(from);
-              //   await sendWhatsAppMessage(
-              //     from,
-              //     "Subagency is restricted. For better service, please contact 88662 65662 on WhatsApp.",
-              //     "en"
-              //   );
-              //   await sendWhatsAppMessage(
-              //     from,
-              //     "सब एजेंसी प्रतिबंधित है। बेहतर सेवा के लिए कृपया 88662 65662 पर WhatsApp पर संपर्क करें।",
-              //     "hi"
-              //   );
-              //   await sendWhatsAppMessage(
-              //     from,
-              //     "સબએજન્સી પ્રતિબંધિત છે. વધુ સારી સેવા માટે, કૃપા કરીને WhatsApp પર 88662 65662 પર સંપર્ક કરો.",
-              //     "gu"
-              //   );
-              // } else {
-              await sendInteractiveMessage(from, [
-                formattedVehicleNumber,
-                response.data[0].lattitude,
-                response.data[0].longitude,
-                response.data[0].speed,
-                response.data[0].received_Date,
-                response.data[0].servertime,
-              ]);
-              userState.step = 2;
-              // }
-            } else {
-              resetUserState(from);
-              await sendWhatsAppMessage(
-                from,
-                "You've reached your weekly limit for vehicle complaints, please try another mobile number to register a complaint.",
-                "en"
-              );
-              await sendWhatsAppMessage(
-                from,
-                "आप वाहन शिकायतों के लिए अपनी साप्ताहिक सीमा तक पहुँच गए हैं, कृपया शिकायत दर्ज करने के लिए कोई अन्य मोबाइल नंबर आज़माएँ।",
-                "hi"
-              );
-              await sendWhatsAppMessage(
-                from,
-                "તમે વાહનની ફરિયાદો માટે તમારી સાપ્તાહિક મર્યાદા સુધી પહોંચી ગયા છો, કૃપા કરીને બીજા મોબાઈલ નંબર થી ફરિયાદ દાખલ કરો.",
-                "gu"
-              );
-            }
-            //
-          }
-        }
-      } else if (userState.step === 2) {
-        const buttonId = message.interactive.button_reply.id;
-        if (buttonId === "update_button") {
-          await sendLocationRequest(from);
-          userState.step = 3;
-        } else {
-          userState.locationAttempts += 1;
-          if (userState.locationAttempts >= 3) {
             resetUserState(from);
             await sendWhatsAppMessage(
               from,
-              "You have exceeded the allowed attempts. Send 'Hi' to start the conversation.",
+              "You've reached your weekly limit for vehicle complaints, please try another mobile number to register a complaint.",
               "en"
             );
             await sendWhatsAppMessage(
               from,
-              "आपने अनुमत प्रयासों को पार कर लिया है। 'Hi' भेजकर बातचीत शुरू करें।",
+              "आप वाहन शिकायतों के लिए अपनी साप्ताहिक सीमा तक पहुँच गए हैं, कृपया शिकायत दर्ज करने के लिए कोई अन्य मोबाइल नंबर आज़माएँ।",
               "hi"
             );
             await sendWhatsAppMessage(
               from,
-              "તમે અનુમતિ આપેલા પ્રયત્નો પાર કરી દીધા છે. 'Hi' મોકલીને સંવાદ શરૂ કરો.",
+              "તમે વાહનની ફરિયાદો માટે તમારી સાપ્તાહિક મર્યાદા સુધી પહોંચી ગયા છો, કૃપા કરીને બીજા મોબાઈલ નંબર થી ફરિયાદ દાખલ કરો.",
               "gu"
             );
-          } else {
-            await sendWhatsAppMessage(from, `Invalid option.`, "en");
-            await sendWhatsAppMessage(from, `अमान्य विकल्प।`, "hi");
-            await sendWhatsAppMessage(from, `અમાન્ય વિકલ્પ.`, "gu");
           }
+          //
         }
-      } else if (userState.step === 3) {
-        if (message.location) {
-          const { latitude, longitude } = message.location;
-          userState.latitude = parseFloat(latitude).toFixed(6);
-          userState.longitude = parseFloat(longitude).toFixed(6);
-          await submitComplaint(from, userState);
-          resetUserState(from);
-        } else {
-          userState.locationAttempts += 1;
-          if (userState.locationAttempts >= 3) {
-            resetUserState(from);
-            await sendWhatsAppMessage(
-              from,
-              "You have exceeded the allowed attempts. Send 'Hi' to start the conversation.",
-              "en"
-            );
-            await sendWhatsAppMessage(
-              from,
-              "आपने अनुमत प्रयासों को पार कर लिया है। 'Hi' भेजकर बातचीत शुरू करें।",
-              "hi"
-            );
-            await sendWhatsAppMessage(
-              from,
-              "તમે અનુમતિ આપેલા પ્રયત્નો પાર કરી દીધા છે. 'Hi' મોકલીને સંવાદ શરૂ કરો.",
-              "gu"
-            );
-          } else {
-            await sendWhatsAppMessage(
-              from,
-              `Please share a valid location.`,
-              "en"
-            );
-            await sendWhatsAppMessage(
-              from,
-              `कृपया एक मान्य स्थान साझा करें।`,
-              "hi"
-            );
-            await sendWhatsAppMessage(from, `કૃપયા માન્ય સ્થાન શેર કરો.`, "gu");
-          }
-        }
-      } else {
-        resetUserState(from);
-        await sendWhatsAppMessage(
-          from,
-          "Sorry, I didn't understand that. Send 'Hi' to start the conversation.",
-          "en"
-        );
-        await sendWhatsAppMessage(
-          from,
-          "मुझे खेद है, मुझे यह समझ में नहीं आया। 'Hi' भेजकर बातचीत शुरू करें।",
-          "hi"
-        );
-        await sendWhatsAppMessage(
-          from,
-          "મને ખેદ છે, મને તે સમજાયું નથી. 'Hi' મોકલીને સંવાદ શરૂ કરો.",
-          "gu"
-        );
       }
+    } else if (userState.step === 2) {
+      const buttonId = message.interactive.button_reply.id;
+      if (buttonId === "update_button") {
+        await sendLocationRequest(from);
+        userState.step = 3;
+      } else {
+        userState.locationAttempts += 1;
+        if (userState.locationAttempts >= 3) {
+          resetUserState(from);
+          await sendWhatsAppMessage(
+            from,
+            "You have exceeded the allowed attempts. Send 'Hi' to start the conversation.",
+            "en"
+          );
+          await sendWhatsAppMessage(
+            from,
+            "आपने अनुमत प्रयासों को पार कर लिया है। 'Hi' भेजकर बातचीत शुरू करें।",
+            "hi"
+          );
+          await sendWhatsAppMessage(
+            from,
+            "તમે અનુમતિ આપેલા પ્રયત્નો પાર કરી દીધા છે. 'Hi' મોકલીને સંવાદ શરૂ કરો.",
+            "gu"
+          );
+        } else {
+          await sendWhatsAppMessage(from, `Invalid option.`, "en");
+          await sendWhatsAppMessage(from, `अमान्य विकल्प।`, "hi");
+          await sendWhatsAppMessage(from, `અમાન્ય વિકલ્પ.`, "gu");
+        }
+      }
+    } else if (userState.step === 3) {
+      if (message.location) {
+        const { latitude, longitude } = message.location;
+        userState.latitude = parseFloat(latitude).toFixed(6);
+        userState.longitude = parseFloat(longitude).toFixed(6);
+        await submitComplaint(from, userState);
+        resetUserState(from);
+      } else {
+        userState.locationAttempts += 1;
+        if (userState.locationAttempts >= 3) {
+          resetUserState(from);
+          await sendWhatsAppMessage(
+            from,
+            "You have exceeded the allowed attempts. Send 'Hi' to start the conversation.",
+            "en"
+          );
+          await sendWhatsAppMessage(
+            from,
+            "आपने अनुमत प्रयासों को पार कर लिया है। 'Hi' भेजकर बातचीत शुरू करें।",
+            "hi"
+          );
+          await sendWhatsAppMessage(
+            from,
+            "તમે અનુમતિ આપેલા પ્રયત્નો પાર કરી દીધા છે. 'Hi' મોકલીને સંવાદ શરૂ કરો.",
+            "gu"
+          );
+        } else {
+          await sendWhatsAppMessage(
+            from,
+            `Please share a valid location.`,
+            "en"
+          );
+          await sendWhatsAppMessage(
+            from,
+            `कृपया एक मान्य स्थान साझा करें।`,
+            "hi"
+          );
+          await sendWhatsAppMessage(from, `કૃપયા માન્ય સ્થાન શેર કરો.`, "gu");
+        }
+      }
+    } else {
+      resetUserState(from);
+      await sendWhatsAppMessage(
+        from,
+        "Sorry, I didn't understand that. Send 'Hi' to start the conversation.",
+        "en"
+      );
+      await sendWhatsAppMessage(
+        from,
+        "मुझे खेद है, मुझे यह समझ में नहीं आया। 'Hi' भेजकर बातचीत शुरू करें।",
+        "hi"
+      );
+      await sendWhatsAppMessage(
+        from,
+        "મને ખેદ છે, મને તે સમજાયું નથી. 'Hi' મોકલીને સંવાદ શરૂ કરો.",
+        "gu"
+      );
     }
   } catch (error) {
     console.error("Error:", error);
@@ -380,8 +371,22 @@ exports.handleMessage = async (req, res) => {
       "gu"
     );
   }
+};
 
-  //   res.sendStatus(200);
+const checkBlockStatus = async (phoneNumber) => {
+  try {
+    const query = "SELECT blocked FROM users WHERE phone_number = ? LIMIT 1";
+    const [results] = await db.promise().query(query, [phoneNumber]);
+
+    if (results.length === 0) {
+      return false; // Number not found
+    }
+
+    return results[0].blocked === 1; // Return true if the user is blocked
+  } catch (error) {
+    console.error("Error checking block status:", error);
+    throw new Error("Database error");
+  }
 };
 
 // Utility: Get current week number
@@ -414,11 +419,8 @@ async function weekCheck(vehicleNumber, mobileNumber, currentWeek, limit) {
       const vehicleCount = countResult[0].vehicle_count;
       if (vehicleCount >= limit) {
         // User has already registered two vehicles this week
-        console.log(limit + "limit");
-        console.log("Limit Error");
         return false; // Limit reached, return false
       } else {
-        console.log(limit + "limit");
         // Step 3: Register the new vehicle
         await db.query(
           "INSERT INTO weekly_data (vehicle_number, mobile_number, week, created_at) VALUES (?, ?, ?, NOW())",
@@ -430,7 +432,6 @@ async function weekCheck(vehicleNumber, mobileNumber, currentWeek, limit) {
       }
     }
   } catch (err) {
-    console.error(err);
     return { message: "Database error.", error: err }; // Handle any errors
   }
 }
@@ -979,6 +980,3 @@ async function submitComplaint(from, userState) {
     );
   }
 }
-
-//   const port = process.env.PORT || 3000;
-//   app.listen(port, () => console.log(`Server running on port ${port}`));
