67fcea860d705e5f4d07aa13237e8c274e2367e0
Jaimik Patel <jaimik1603@gmail.com>
2025-01-18 01:33:35 +0530

    ff

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const PHONE_NUMBER_1 = "469434999592396";
 const PHONE_NUMBER_2 = "532876653233461";
 
 app.post("/webhook", (req, res) => {
+  console.log(req.body);
   const body = req.body;
 
   if (body && body.entry && body.entry.length > 0) {
