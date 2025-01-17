3e0b4cf9dc6f5c7f1a50d62481b68d3daa5e8ce6
Jaimik Patel <jaimik1603@gmail.com>
2025-01-18 02:13:26 +0530

    dd

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const PHONE_NUMBER_1 = "469434999592396";
 const PHONE_NUMBER_2 = "532876653233461";
 
 app.post("/webhook", (req, res) => {
-  const body = req.body;
+  const body = req.body.whatsapp_webhook_payload;
   // console.log(body);
   // console.log(body.whatsapp_webhook_payload);
   console.log("Full Data:", JSON.stringify(body.whatsapp_webhook_payload, null, 2));
